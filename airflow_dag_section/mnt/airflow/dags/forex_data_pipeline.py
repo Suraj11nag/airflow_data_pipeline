03e3673440a223da6781a0d8139c64d7476131a9
Suraj Nag <suraj11nag@gmail.com>
2024-01-02 23:25:24 +0530

    added operator

--- airflow_dag_section/mnt/airflow/dags/forex_data_pipeline.py
+++ airflow_dag_section/mnt/airflow/dags/forex_data_pipeline.py
@@ -1,5 +1,6 @@
 # Initialize a dag
 from airflow import DAG
+from airflow.providers.http.sensors.http import HttpSensor
 from datetime import datetime, timedelta
 
 default_args = {
@@ -11,5 +12,14 @@ default_args = {
     "retry_delay": timedelta(minutes=5)
 }
 
-with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1), 
+with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1),
          schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
+    
+    is_forex_rates_available = HttpSensor(
+        task_id="is_forex_rates_available",
+        http_con_id="forex_api",
+        endpoint="marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b",
+        response_check=lambda response: "rates" in response.text,
+        poke_interval=5, #checking the url forex is available or not
+        Timeout=20
+    )
