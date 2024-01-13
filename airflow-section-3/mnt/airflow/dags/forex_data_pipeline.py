a1087d838e76ff13634dfec0a3c8edf870d6b011
Suraj Nag <suraj11nag@gmail.com>
2024-01-13 18:36:26 +0530

    update

--- airflow-section-3/mnt/airflow/dags/forex_data_pipeline.py
+++ airflow-section-3/mnt/airflow/dags/forex_data_pipeline.py
@@ -3,6 +3,8 @@ from airflow.providers.http.sensors.http import HttpSensor
 from airflow.sensors.filesystem import FileSensor
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
+from airflow.providers.apache.hive.operators.hive import HiveOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 from datetime import datetime, timedelta
 import csv
@@ -80,4 +82,32 @@ with DAG("forex_data_pipeline",
             hdfs dfs -mkdir -p /forex && \
             hdfs dfs -put -f $AIRFLOW_HOME/dags/files/forex_rates.json /forex
             """
+    )
+
+    # creating a task
+    creating_forex_rates_table = HiveOperator(
+        task_id="creating_forex_rates_table",
+        hive_cli_conn_id="hive_conn",
+        hql="""
+            CREATE EXTERNAL TABLE IF NOT EXISTS forex_rates(
+                base STRING,
+                last_update DATE,
+                eur DOUBLE,
+                usd DOUBLE,
+                nzd DOUBLE,
+                gbp DOUBLE,
+                jpy DOUBLE,
+                cad DOUBLE
+                )
+            ROW FORMAT DELIMITED
+            FIELDS TERMINATED BY ','
+            STORED AS TEXTFILE
+        """
+    )
+
+    forex_processing = SparkSubmitOperator(
+        task_id="forex_processing",
+        application="/opt/airflow/dags/scripts/forex_processing.py",
+        conn_id="spark_conn",
+        verbose=False
     )
